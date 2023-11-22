97b06429533310284eb0f3a2ece6f557a6392fcd
Saman A. Barakat <saman.barakat@gmail.com>
2023-11-22 15:58:39 +0100

    IDL Gateway Project Updated

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,2 +1,1 @@
-#! /bin/bash
-mvn install:install-file -Dfile=lib/idlreasoner-choco-1.0.0.jar -DgroupId=es.us.isa -DartifactId=idlreasoner-choco -Dversion=1.0.0 -Dpackaging=jar
+mvn install:install-file -Dfile=lib/IDLReasoner.jar -DgroupId=es.us.isa -DartifactId=IDLReasoner -Dversion=1.0.1 -Dpackaging=jar
