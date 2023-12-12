d217924531cc8a1a578ba1d722875c29cd796a08
Saman A. Barakat <saman.barakat@gmail.com>
2023-12-12 15:18:02 +0100

    IDLFilter Updated

--- src/main/java/es/us/isa/idl/apigateway/filters/IDLFilter.java
+++ src/main/java/es/us/isa/idl/apigateway/filters/IDLFilter.java
@@ -20,23 +20,17 @@ import java.util.Map;
 
 @Component
 public class IDLFilter extends AbstractGatewayFilterFactory<IDLFilter.Config> {
-    final Logger logger =
-            LoggerFactory.getLogger(IDLFilter.class);
-    private static final String ANALYSIS = "analysis";
+    final Logger logger = LoggerFactory.getLogger(IDLFilter.class);
+
     public IDLFilter() {
         super(Config.class);
         logger.info("IDLFilter constructor");
     }
 
-    @Value("${app.RequestType}")
-    private String RequestType;
     private static final String BASE_SPEC_PATH = "./src/test/resources/GatewayExperiment";
     private static final String BASE_CSV_PATH = "./src/test/resources/GatewayExperiment/Performance";
 
-    @Override
-    public List<String> shortcutFieldOrder() {
-        return Collections.singletonList(ANALYSIS);
-    }
+
     @Override
     public GatewayFilter apply(Config config) {
 
@@ -46,103 +40,23 @@ public class IDLFilter extends AbstractGatewayFilterFactory<IDLFilter.Config> {
 
             Long startTime = System.nanoTime();
 
-            String serviceName;
-            String operationPath;
-            String SPEC_URL;
-            String csvFilePath;
-            String requestPath = exchange.getRequest().getPath().toString();
+            String serviceName = config.serviceName;
+            String operationPath = config.operationPath;
+            String SPEC_URL = BASE_SPEC_PATH + config.specPath;
+            String csvFilePath = BASE_CSV_PATH + config.csvPath;
+
             String operationType = exchange.getRequest().getMethod().name().toLowerCase();
             Map<String, String> paramMap = exchange.getRequest().getQueryParams().toSingleValueMap();
 
-            if(requestPath.contains("businesses")) {
-                serviceName = "Yelp";
-                operationPath = "/businesses/search";
-                SPEC_URL = BASE_SPEC_PATH + "/YelpBusinessesSearch/openapi.yaml";
-                csvFilePath = BASE_CSV_PATH + "/"+ RequestType +"/YelpBusinessesSearch/YelpBusinessesSearch" + config.analysis + ".csv";
-            }
-            else if(requestPath.contains("transactions")) {
-                serviceName = "Yelp";
-                operationPath = "/transactions/{transaction_type}/search";
-                paramMap.put("transaction_type","delivery");
-                SPEC_URL = BASE_SPEC_PATH + "/YelpTransactionsSearch/openapi.yaml";
-                csvFilePath = BASE_CSV_PATH + "YelpTransactionsSearch/YelpTransactionsSearch" + config.analysis + ".csv";
-            }
-            else if(requestPath.contains("complexSearch")) {
-                serviceName = "Spoonacular";
-                operationPath = "/recipes/complexSearch";
-
-                SPEC_URL = BASE_SPEC_PATH + "/Spoonacular/openapi.yaml";
-                csvFilePath = BASE_CSV_PATH + "/Spoonacular.csv";
-            }
-            else if(requestPath.contains("folders")) {
-                serviceName = "Box";
-                operationPath = "/folders/{folder_id}/items";
-                String[] parts = requestPath.split("/");
-                String value = parts[3];
-                paramMap.put("folder_id",value);
-                SPEC_URL = BASE_SPEC_PATH + "/Box/openapi.yaml";
-                csvFilePath = BASE_CSV_PATH + "/Box/IDLDetection.csv";
-
-            }
-            else if(requestPath.contains("repos")) {
-                serviceName = "GitHub";
-                operationPath = "/user/repos";
-                SPEC_URL = BASE_SPEC_PATH + "/GitHub/openapi.yaml";
-                csvFilePath = BASE_CSV_PATH + "/GitHub.csv";
-            }
-            else if(paramMap.get("apikey").equals("9c07f6df")) {
-                serviceName = "OMDb";
-                operationPath = "/";
-                paramMap.remove("apikey");
-
-                SPEC_URL = BASE_SPEC_PATH + "/swagger_byIdOrTitle.yaml";
-                csvFilePath = BASE_CSV_PATH + "/OMDb/IDLDetection.csv";
-            }
-            else if(requestPath.contains("places")) {
-                serviceName = "FSQ";
-                operationPath = "/places/search";
-                SPEC_URL = BASE_SPEC_PATH + "/Foursquare/openapi.yaml";
-                csvFilePath = BASE_CSV_PATH + "/FSQ.csv";
-            }
-            else if(requestPath.contains("flight-offers")) {
-                serviceName = "AmadeusFlight";
-                operationPath = "/shopping/flight-offers";
-
-                SPEC_URL = BASE_SPEC_PATH + "/AmadeusFlight/openapi.yaml";
-                csvFilePath = BASE_CSV_PATH + "/AmadeusFlight.csv";
-            }
-            else if(requestPath.contains("hotel-offers")) {
-                serviceName = "AmadeusHotel";
-                operationPath = "/shopping/hotel-offers";
-
-                SPEC_URL = BASE_SPEC_PATH + "/AmadeusHotel/swagger.yaml";
-                csvFilePath = BASE_CSV_PATH + "/AmadeusHotel.csv";
-            }
-            else if(requestPath.contains("address")) {
-                serviceName = "DHL";
-
-                operationPath = "/find-by-address";
-                SPEC_URL = BASE_SPEC_PATH + "/DHL/openapi.yaml";
-                csvFilePath = BASE_CSV_PATH + "/DHL.csv";
-            }
-            else if(requestPath.contains("blog")) {
-                serviceName = "Tumblr";
-                operationPath = "/blog/{blog-identifier}/likes";
-                paramMap.remove("api_key");
-                paramMap.put("blog-identifier","reasonerapigateway.tumblr.com");
-
-                SPEC_URL = BASE_SPEC_PATH + "/Tumblr/swagger.yaml";
-                csvFilePath = BASE_CSV_PATH + "/Tumblr.csv";
-            }
-            else {
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Path did not match!");
-            }
+System.out.println("SPEC_URL: " + SPEC_URL);
+System.out.println("operationPath: " + operationPath);
+System.out.println("operationType: " + operationType);
+System.out.println("paramMap: " + paramMap.toString());
 
             CSVManager csvManager = new CSVManager(csvFilePath, serviceName,operationPath,paramMap.toString());
 
             try {
                 double analysisTime;
-             //   Long analysisStartTime = System.nanoTime();
 
                 if (config.analysis.equals("Detection") || config.analysis.equals("Explanation")) {
 
@@ -178,7 +92,6 @@ public class IDLFilter extends AbstractGatewayFilterFactory<IDLFilter.Config> {
 
                 return chain.filter(exchange).then(Mono.fromRunnable(() -> {
 
-
                     Long serverResponseEndTime = System.nanoTime();
                     double serverResponseTime = (serverResponseEndTime - serverResponseStartTime)/ 1000000.0;
 
@@ -197,6 +110,39 @@ public class IDLFilter extends AbstractGatewayFilterFactory<IDLFilter.Config> {
 
     public static class Config {
         private String analysis;
+        private String specPath;
+        private String csvPath;
+        private String serviceName;
+
+        public String getOperationPath() {
+            return operationPath;
+        }
+
+        public void setOperationPath(String operationPath) {
+            this.operationPath = operationPath;
+        }
+
+        private String operationPath;
+
+        public String getSpecPath() {
+            return specPath;
+        }
+        public void setSpecPath(String specPath) {
+            this.specPath = specPath;
+        }
+        public String getCsvPath() {
+            return csvPath;
+        }
+        public void setCsvPath(String csvPath) {
+            this.csvPath = csvPath;
+        }
+
+        public String getServiceName() {
+            return serviceName;
+        }
+          public void setServiceName(String serviceName) {
+                this.serviceName = serviceName;
+          }
 
         public String getAnalysis() {
             return analysis;
